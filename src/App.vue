625cdb063521698600a03f975e9243b2bea7f10e
Nguyen Huu Thuat <thuatnguyen09112002@gmail.com>
2022-10-19 20:10:17 +0700

    thuat-up

---
+++ src/App.vue
@@ -0,0 +1,384 @@
+<style>
+
+</style>
+
+<template>
+  <div class="mainContent">
+    <ul class="list_item">
+      <li class="item_custom" v-for="(item, index) in state.data" :key="index">
+        <button
+          class="btn_dot"
+          @click="handleChoice(index)"
+          :disabled="isDisabled"
+          :class="{ red: item == 1 && item != 0, blue: item != 1 && item != 0 }"
+        >
+          {{ item == 0 ? "" : item == 1 ? "o" : "x" }}
+          <p v-if="checkShow(index)">oke</p>
+        </button>
+      </li>
+    </ul>
+    <div class="container-handler">
+      <p class="textShow">{{ textShow }}</p>
+    </div>
+      <button class="button-85" @click="resetHandler">Reset game</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      player: 1,
+      turn: 0,
+      state: {
+        data: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        size: 3,
+      },
+      textShow: "",
+      isDisabled: false,
+      stateWin: {
+        index: null,
+        form: "",
+      },
+    };
+  },
+  created() {
+    // this.checkDraw();
+  },
+  methods: {
+    handleChoice(index) {
+      if ((this.turn % 2) + 1 == this.player) {
+        let dataState = this.state;
+        if (dataState != null) {
+          if (this.state.data[index] == 0) {
+            this.state.data[index] = 1;
+          }
+          if (this.Win(this.state)) {
+            this.textShow = "Player Won";
+            console.log("Player won");
+            this.isDisabled = true;
+            return;
+          }
+        }
+      }
+
+      if (this.checkDraw()) {
+        return;
+      }
+      this.turn += 1;
+    },
+
+    aiHandleChoice() {
+      console.log("vao ai ");
+      let mn = 2;
+      let minChild = null;
+      let sz = this.state.size;
+      let child;
+
+      for (let i = 0; i < sz; i++) {
+        for (let j = 0; j < sz; j++) {
+          child = this.move(this.state, i, j);
+          console.log("tim child:", child);
+          if (child == null) {
+            continue;
+          }
+          let tmp = this.MiniMax(child, 1, true);
+          console.log(i, j, tmp);
+          if (mn > tmp) {
+            mn = tmp;
+            minChild = child;
+          }
+        }
+      }
+      console.log("tim ra minchild: ", minChild);
+      this.state = minChild;
+      if (this.Win(this.state)) {
+        this.textShow = "AI Won";
+        console.log("AI won");
+        this.isDisabled = true;
+        return;
+      }
+      if (this.checkDraw()) {
+        return;
+      }
+      this.turn += 1;
+    },
+
+    checkDraw() {
+      let check = false;
+      if (this.isEndNode(this.state)) {
+        console.log("Match Drawn");
+        this.textShow = "Match Drawn";
+        this.isDisabled = true;
+        check = true;
+      }
+      return check;
+    },
+
+    move(state, x, y) {
+      let sz = state.size;
+      if (x < 0 || x >= sz) {
+        return null;
+      }
+
+      if (y < 0 || y >= sz) {
+        return null;
+      }
+
+      if (state.data[x * sz + y] != 0) {
+        return null;
+      }
+
+      let res = 0;
+      for (const value of state.data) {
+        if (value != 0) {
+          res += 1;
+        }
+      }
+
+      let sn = JSON.parse(JSON.stringify(state));
+      if (res % 2 == 0) {
+        sn.data[x * sz + y] = 1;
+      } else {
+        sn.data[x * sz + y] = 2;
+      }
+      return sn;
+    },
+
+// Cắt tỉa alpha-beta
+    MiniMax(s, d, mp) {
+      // giá trị của mỗi node là 0 1 -1 nên apha = -2 và beta = 2
+      return this.AlphaBeta(s, d, -2, 2, mp);
+    },
+
+    AlphaBeta(s, d, a, b, mp) {
+      console.log("vao s", s);
+      // neu node là nút lá hoặc độ sâu d = 0 thì trả về giá trí của node đó là thắng thua hoặc hoà
+      if (this.isEndNode(s) || d == 0) {
+        return this.Value(s);
+      }
+      let sz = s.size;
+
+// nếu đang ở trạng thái max thì duyệt qua tất cả các đỉnh con
+      if (mp) {
+        for (let i = 0; i < sz; i++) {
+          for (let j = 0; j < sz; j++) {
+            let child = this.move(s, i, j);
+            if (child == null) {
+              continue;
+            }
+            let tmp = this.AlphaBeta(child, d - 1, a, b, false);
+            // nếu node = true thì đang ở max, nếu đanng ở maximum thì phải truyền vào false(các node của nó đang ở trạng thái min)
+            // gọi a là maximum của tất cả các đỉnh con của node hiện tại
+            a = Math.max(a, tmp);
+            // nếu a > = b thì xoá luôn nút đó
+            if (a >= b) {
+              break;
+            }
+          }
+        }
+        return a;
+      } else {
+        for (let i = 0; i < sz; i++) {
+          for (let j = 0; j < sz; j++) {
+            let child = this.move(s, i, j);
+            if (child == null) {
+              continue;
+            }
+            let tmp = this.AlphaBeta(child, d - 1, a, b, true);
+            a = Math.min(a, tmp);
+            if (a >= b) {
+              break;
+            }
+          }
+        }
+        return b;
+      }
+    },
+
+    Value(s) {
+      if (this.Win(s)) {
+        if (this.checkMyTurn(s)) {
+          return 1;
+        }
+        return -1;
+      }
+      return 0;
+    },
+
+    Win(s) {
+      if (s == null) {
+        return false;
+      }
+
+      let sz = s.size;
+      let data = s.data;
+
+      for (let i = 0; i < sz; i++) {
+        // check row
+        if (
+          data[i * sz + 0] != 0 &&
+          data[i * sz + 0] == data[i * sz + 1] &&
+          data[i * sz + 0] == data[i * sz + 2]
+        ) {
+          // this.row = i;
+          this.stateWin = {
+            index: i,
+            form: "row",
+          };
+          return true;
+        }
+
+        // check column
+        if (
+          data[0 * sz + i] != 0 &&
+          data[0 * sz + i] == data[1 * sz + i] &&
+          data[0 * sz + i] == data[2 * sz + i]
+        ) {
+          // this.column = i;
+          this.stateWin = {
+            index: i,
+            form: "column",
+          };
+          return true;
+        }
+      }
+
+      if (
+        data[0 * sz + 0] != 0 &&
+        data[0 * sz + 0] == data[1 * sz + 1] &&
+        data[0 * sz + 0] == data[2 * sz + 2]
+      ) {
+        this.stateWin = {
+          index: null,
+          form: "main",
+        };
+        return true;
+      }
+      if (
+        data[0 * sz + 2] != 0 &&
+        data[0 * sz + 2] == data[1 * sz + 1] &&
+        data[0 * sz + 2] == data[2 * sz + 0]
+      ) {
+        this.stateWin = {
+          index: null,
+          form: "sub",
+        };
+        return true;
+      }
+
+      return false;
+    },
+
+    checkMyTurn(s) {
+      let res = 0;
+      for (let x of s.data) {
+        if (x == 0) {
+          res += 1;
+        }
+      }
+      if (res % 2 == 0) {
+        return true;
+      }
+      return false;
+    },
+
+    isEndNode(s) {
+      let sz = s.size;
+      let data = s.data;
+      for (let i = 0; i < sz; i++) {
+        if (
+          data[i * sz + 0] != 0 &&
+          data[i * sz + 0] == data[i * sz + 1] &&
+          data[i * sz + 0] == data[i * sz + 2]
+        ) {
+          return true;
+        }
+
+        if (
+          data[0 * sz + i] != 0 &&
+          data[0 * sz + i] == data[1 * sz + i] &&
+          data[0 * sz + i] == data[2 * sz + i]
+        ) {
+          return true;
+        }
+      }
+
+      if (
+        data[0 * sz + 0] != 0 &&
+        data[0 * sz + 0] == data[1 * sz + 1] &&
+        data[0 * sz + 0] == data[2 * sz + 2]
+      ) {
+        return true;
+      }
+      if (
+        data[0 * sz + 2] != 0 &&
+        data[0 * sz + 2] == data[1 * sz + 1] &&
+        data[0 * sz + 2] == data[2 * sz + 0]
+      ) {
+        return true;
+      }
+
+      for (const value of data) {
+        if (value == 0) {
+          return false;
+        }
+      }
+      return true;
+    },
+
+    resetHandler() {
+      this.player = 1;
+      this.turn = 0;
+      this.state = {
+        data: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        size: 3,
+      };
+      this.textShow = "";
+      this.isDisabled = false;
+      this.stateWin = {
+        index: null,
+        form: "",
+      };
+    },
+
+    checkShow(index) {
+      let indexState = this.stateWin.index;
+      const sz = this.state.size;
+      if (this.textShow != "" && this.textShow != "Match Drawn") {
+        if (this.stateWin.form == "column") {
+          let arrR = [
+            0 * sz + indexState,
+            1 * sz + indexState,
+            2 * sz + indexState,
+          ];
+          return arrR.includes(index);
+        } else if (this.stateWin.form == "row") {
+          let arrR = [
+            indexState * sz + 0,
+            indexState * sz + 1,
+            indexState * sz + 2,
+          ];
+          return arrR.includes(index);
+        } else if (this.stateWin.form == "main") {
+          let arrR = [0 * sz + 0, 1 * sz + 1, 2 * sz + 2];
+          return arrR.includes(index);
+        } else {
+          let arrR = [2 * sz + 0, 1 * sz + 1, 0 * sz + 2];
+          return arrR.includes(index);
+        }
+      }
+    },
+  },
+  watch: {
+    turn(newVal) {
+      console.log("data", newVal);
+      if ((newVal % 2) + 1 != this.player) {
+        this.aiHandleChoice();
+      }
+    },
+  },
+};
+</script>
